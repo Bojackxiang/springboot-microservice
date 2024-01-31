7e30166cd53328c092d534a8d990031c0b4cfe5d
Alex <alex@q-83.com>
2024-01-31 14:24:17 +1100

    path variable added

--- src/main/java/com/alex/restfulwebservice/controller/HelloWorld.java
+++ src/main/java/com/alex/restfulwebservice/controller/HelloWorld.java
@@ -2,8 +2,7 @@ package com.alex.restfulwebservice.controller;
 
 import com.alex.restfulwebservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,13 +10,18 @@ public class HelloWorld {
 
   // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
   @GetMapping(path = "/hello-world")
-  public String HelloWorld(){
+  public String HelloWorld() {
     return "hello world";
   }
 
   @GetMapping(path = "/hello-world-bean")
-  public HelloWorldBean HelloWorldBean(){
+  public HelloWorldBean HelloWorldBean() {
     return new HelloWorldBean("Hello-world");
+  }
+
+  @GetMapping(path = "/hello-world/path-variable/{name}")
+  public HelloWorldBean HelloWorldPathVariable(@PathVariable String name) {
+    return new HelloWorldBean(name);
   }
 
 }
