e1c8cffd6e5663afe6a0222c7941ba3405cb8a82
Alex <alex@q-83.com>
2024-02-02 15:19:48 +1100

    adding command runner

---
+++ src/main/java/com/alex/restfulwebservice/customCommandRunner/CustomRunner1.java
@@ -0,0 +1,13 @@
+package com.alex.restfulwebservice.customCommandRunner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRunner1 implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("CustomRunner1.run");
+  }
+}
